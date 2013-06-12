71b0a5f295b4f00cac6644afc20cd00b5984858e
nicholas.delrio <nicholas.delrio@gmail.com>
2013-06-12 17:36:41 -0600

    included json serializing/deserializing classes

---
+++ ogcrdf-v3/src/main/java/edu/utep/cybershare/elseweb/ogc/wcs/WCSDigest.java
@@ -0,0 +1,29 @@
+package edu.utep.cybershare.elseweb.ogc.wcs;
+
+import java.net.URL;
+
+public class WCSDigest {
+	
+	private int lastUpdate;
+	private String uuid;
+	private String taxonomy;
+	private URL tiffDownload;
+	private String description;
+	private URL FGDC_metadataXML;
+	private URL FGDC_metadataHTML;
+	private String projectionCode;
+	private double leftLongitude;
+	private double rightLongitude;
+	private double lowerLatitude;
+	private double upperLatitude;
+	private URL wmsService;
+	private URL wcsService;
+	private URL preview;
+	private int gr;
+	private String subtheme;
+	private String theme;
+	
+	
+	
+
+}
