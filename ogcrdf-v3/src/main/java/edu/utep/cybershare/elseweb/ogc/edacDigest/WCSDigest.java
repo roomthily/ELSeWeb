bf8a0e1c5b974b052508116090022f6bfa3c3618
Nick <nicholas.delrio@gmail.com>
2013-06-13 12:03:53 -0600

    completely parsed json file

--- ogcrdf-v3/src/main/java/edu/utep/cybershare/elseweb/ogc/edacDigest/WCSDigest.java
+++ ogcrdf-v3/src/main/java/edu/utep/cybershare/elseweb/ogc/edacDigest/WCSDigest.java
@@ -2,6 +2,7 @@ package edu.utep.cybershare.elseweb.ogc.edacDigest;
 
 import java.net.URL;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class WCSDigest {
@@ -13,7 +14,7 @@ public class WCSDigest {
 	private String description;
 	private URL FGDC_metadataXML;
 	private URL FGDC_metadataHTML;
-	private String projectionCode;
+	private int epsg;
 	private double leftLongitude;
 	private double rightLongitude;
 	private double lowerLatitude;
@@ -24,124 +25,186 @@ public class WCSDigest {
 	private int gr;
 	private String subtheme;
 	private String theme;
-	private String groupName;
+	private String groupname;
+	
+	private JSONObject jsonDigest;
 	
 	public WCSDigest(JSONObject wcsJSONDigest){
+		jsonDigest = wcsJSONDigest;
 		
+		this.setCategories();
+		this.setDescription();
+		this.setLastUpdate();
+		this.setMetadata();
+		this.setPreview();
+		this.setServices();
+		this.setSpatial();
+		this.setTaxonomy();
+		this.setDownloads();
+		this.setUuid();
 	}
 	
 	public int getLastUpdate() {
 		return lastUpdate;
 	}
-	protected void setLastUpdate(int lastUpdate) {
-		this.lastUpdate = lastUpdate;
+	private void setLastUpdate() {
+		try{this.lastUpdate =  Integer.valueOf(jsonDigest.getString("lastupdate"));}
+		catch(Exception e){e.printStackTrace();}
 	}
 	public String getUuid() {
 		return uuid;
 	}
-	protected void setUuid(String uuid) {
-		this.uuid = uuid;
+	private void setUuid() {
+		try{this.uuid =  jsonDigest.getString("lastupdate");}
+		catch(Exception e){e.printStackTrace();}
 	}
+
 	public String getTaxonomy() {
 		return taxonomy;
 	}
-	protected void setTaxonomy(String taxonomy) {
-		this.taxonomy = taxonomy;
+	private void setTaxonomy() {
+		try{this.taxonomy =  jsonDigest.getString("lastupdate");}
+		catch(Exception e){e.printStackTrace();}
 	}
+
 	public URL getTiffDownload() {
 		return tiffDownload;
 	}
-	protected void setTiffDownload(URL tiffDownload) {
-		this.tiffDownload = tiffDownload;
+
+	private void setDownloads() {
+		try {
+			JSONArray downloads = jsonDigest.getJSONArray("downloads");
+			JSONObject download0 = downloads.getJSONObject(0);
+			tiffDownload = new URL(download0.getString("tif"));
+		}
+		catch(Exception e){e.printStackTrace();}
 	}
+
 	public String getDescription() {
 		return description;
 	}
-	protected void setDescription(String description) {
-		this.description = description;
+
+	private void setDescription() {
+		try{this.description =  jsonDigest.getString("description");}
+		catch(Exception e){e.printStackTrace();}
 	}
+	
+	private void setMetadata() {
+		try {
+			JSONArray metadata = jsonDigest.getJSONArray("metadata");
+			JSONObject metadata0 = metadata.getJSONObject(0);
+			JSONObject fgdc = metadata0.getJSONObject("fgdc");
+			FGDC_metadataXML = new URL(fgdc.getString("xml"));
+			FGDC_metadataHTML = new URL(fgdc.getString("html"));
+		}
+		catch(Exception e){e.printStackTrace();}
+	}
+
 	public URL getFGDC_metadataXML() {
 		return FGDC_metadataXML;
 	}
-	protected void setFGDC_metadataXML(URL fGDC_metadataXML) {
-		FGDC_metadataXML = fGDC_metadataXML;
-	}
+
 	public URL getFGDC_metadataHTML() {
 		return FGDC_metadataHTML;
 	}
-	protected void setFGDC_metadataHTML(URL fGDC_metadataHTML) {
-		FGDC_metadataHTML = fGDC_metadataHTML;
+	
+	private void setSpatial() {
+		try {
+			JSONObject spatial = jsonDigest.getJSONObject("spatial");
+			epsg = spatial.getInt("epsg");
+			JSONArray bbox = spatial.getJSONArray("bbox");
+			
+			leftLongitude = bbox.getDouble(0);
+			lowerLatitude = bbox.getDouble(1);
+			rightLongitude = bbox.getDouble(2);
+			upperLatitude = bbox.getDouble(3);			
+		}
+		catch(Exception e){e.printStackTrace();}
 	}
-	public String getProjectionCode() {
-		return projectionCode;
+
+	public int getEPSG() {
+		return epsg;
 	}
-	protected void setProjectionCode(String projectionCode) {
-		this.projectionCode = projectionCode;
-	}
+	
 	public double getLeftLongitude() {
 		return leftLongitude;
 	}
-	protected void setLeftLongitude(double leftLongitude) {
-		this.leftLongitude = leftLongitude;
-	}
+
 	public double getRightLongitude() {
 		return rightLongitude;
 	}
-	protected void setRightLongitude(double rightLongitude) {
-		this.rightLongitude = rightLongitude;
-	}
+	
 	public double getLowerLatitude() {
 		return lowerLatitude;
 	}
-	protected void setLowerLatitude(double lowerLatitude) {
-		this.lowerLatitude = lowerLatitude;
-	}
+
 	public double getUpperLatitude() {
 		return upperLatitude;
 	}
-	protected void setUpperLatitude(double upperLatitude) {
-		this.upperLatitude = upperLatitude;
-	}
-	public URL getWmsService() {
-		return wmsService;
-	}
-	protected void setWmsService(URL wmsService) {
-		this.wmsService = wmsService;
+
+	private void setServices() {
+
+		try{
+			JSONArray services = jsonDigest.getJSONArray("services");
+			JSONObject services0 = services.getJSONObject(0);
+			JSONObject services1 = services.getJSONObject(1);
+			
+			wmsService = new URL(services0.getString("wms"));
+			wcsService = new URL(services1.getString("wcs"));
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}		
 	}
 	public URL getWcsService() {
 		return wcsService;
 	}
-	protected void setWcsService(URL wcsService) {
-		this.wcsService = wcsService;
+	
+	public URL getWmsService() {
+		return wmsService;
 	}
+
 	public URL getPreview() {
 		return preview;
 	}
-	protected void setPreview(URL preview) {
-		this.preview = preview;
+	
+	private void setPreview() {
+		try{
+			preview = new URL(jsonDigest.getString("preview"));
+		}catch(Exception e){e.printStackTrace();}
 	}
+	
 	public int getGr() {
 		return gr;
 	}
 	protected void setGr(int gr) {
-		this.gr = gr;
+		try{
+			gr = jsonDigest.getInt("gr");
+		}catch(Exception e){e.printStackTrace();}
 	}
+		
+	private void setCategories() {
+		try{
+			JSONArray categories = jsonDigest.getJSONArray("categories");
+			JSONObject categories0 = categories.getJSONObject(0);
+			theme = categories0.getString("theme");
+			groupname = categories0.getString("groupname");
+			subtheme = categories0.getString("subtheme");			
+		}catch(Exception e){e.printStackTrace();}
+	}
+	
 	public String getSubtheme() {
 		return subtheme;
 	}
-	protected void setSubtheme(String subtheme) {
-		this.subtheme = subtheme;
-	}
+	
 	public String getTheme() {
 		return theme;
 	}
-	protected void setTheme(String theme) {
-		this.theme = theme;
-	}
 	public String getGroupName() {
-		return groupName;
+		return groupname;
 	}
-	protected void setGroupName(String groupName) {
-		this.groupName = groupName;
+	
+	public String toString(){
+		return jsonDigest.toString();
 	}
 }
--- ogcrdf-v3/src/main/java/edu/utep/cybershare/elseweb/ogc/edacDigest/WCSDigests.java
+++ ogcrdf-v3/src/main/java/edu/utep/cybershare/elseweb/ogc/edacDigest/WCSDigests.java
@@ -3,27 +3,34 @@ import static org.apache.commons.io.FileUtils.copyURLToFile;
 import java.io.File;
 import java.io.FileInputStream;
 import java.net.URL;
+import java.util.ArrayList;
 
 import org.apache.commons.io.IOUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-public class WCSDigests {
+public class WCSDigests extends ArrayList<WCSDigest>{
 	
 	private static final String JSON_URL = "http://gstore.unm.edu/apps/elseweb/search/datasets.json?version=3";	
-	
 	
 	public WCSDigests(){
 		JSONArray array = getJSONDigestsArray();
 		
-		
+		try{
+			for(int i = 0; i < array.length(); i ++)
+				addWCSDigest(array.getJSONObject(i));
+		}catch(Exception e){e.printStackTrace();}
 	}
 	
+	private void addWCSDigest(JSONObject wcsJSONDigest){
+		WCSDigest wcsDigest = new WCSDigest(wcsJSONDigest);
+		add(wcsDigest);
+	}
 
 	private JSONArray getJSONDigestsArray(){
 		JSONObject jsonDigests = getJSONDigests();
 		JSONArray jsonDigestsArray = null;
-		try{jsonDigestsArray = (JSONArray)jsonDigests.get("results");}
+		try{jsonDigestsArray = jsonDigests.getJSONArray("results");}
 		catch(Exception e){e.printStackTrace();}
 		return jsonDigestsArray;
 	}
@@ -55,6 +62,7 @@ public class WCSDigests {
 	}
 	
 	public static void main(String[] args){
-		WCSDigests factory = new WCSDigests();
+		WCSDigests digestList = new WCSDigests();
+		System.out.println(digestList);
 	}
 }
