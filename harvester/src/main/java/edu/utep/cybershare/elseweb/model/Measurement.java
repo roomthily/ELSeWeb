24616c7ebfcc678a93364f06f2691922622d070f
Nicholas Del Rio <nicholas.delrio@gmail.com>
2013-09-22 19:43:41 -0700

    made measurement reference observation using inverse property

--- harvester/src/main/java/edu/utep/cybershare/elseweb/model/Measurement.java
+++ harvester/src/main/java/edu/utep/cybershare/elseweb/model/Measurement.java
@@ -4,13 +4,19 @@ import java.net.URI;
 
 public class Measurement extends Element {
 
+	private Observation observation;
+	private Characteristic characteristic;
+	private URI responsibleAgent;
+	
 	public Measurement(String identification) {
 		super(identification);
 		// TODO Auto-generated constructor stub
 	}
-	private Characteristic characteristic;
-	private URI responsibleAgent;
 
+	public boolean isSet_Observation(){
+		return this.getObservation() != null;
+	}
+	
 	public boolean isSet_charactersitc(){
 		return this.getCharacteristic() != null;
 	}
@@ -18,7 +24,15 @@ public class Measurement extends Element {
 	public boolean isSet_responsibleAgent(){
 		return this.getResponsibleAgent() != null;
 	}
-	
+
+	public Observation getObservation() {
+		return observation;
+	}
+
+	public void setObservation(Observation observation) {
+		this.observation = observation;
+	}
+
 	@Override
 	public void accept(Visitor visitor) {
 		// TODO Auto-generated method stub
--- harvester/src/main/java/edu/utep/cybershare/elseweb/model/Observation.java
+++ harvester/src/main/java/edu/utep/cybershare/elseweb/model/Observation.java
@@ -2,30 +2,17 @@ package edu.utep.cybershare.elseweb.model;
 
 public class Observation extends Element {
 
-	private Measurement measurement;
 	private Entity entity;
 	
 	public Observation(String identification) {
 		super(identification);
 		// TODO Auto-generated constructor stub
 	}
-	
-	public boolean isSet_measurement(){
-		return this.getMeasurement() != null;
-	}
-	
+		
 	public boolean isSet_entity(){
 		return this.getEntity() != null;
 	}
 	
-	public Measurement getMeasurement() {
-		return measurement;
-	}
-
-	public void setMeasurement(Measurement measurement) {
-		this.measurement = measurement;
-	}
-
 	public Entity getEntity() {
 		return entity;
 	}
